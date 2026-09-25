7bb04e459f5dab9a9a0b0f2063bc1d647917fad5
LidiaNavarro97 <lnavest97@gmail.com>
2026-09-25 08:56:12 +0200

    Configuración correcta de Vite con Docker

--- practica1/vite.config.js
+++ practica1/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     watch: {
       usePolling: true
     }
   }
-})
+});
